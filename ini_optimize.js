4c1a12ea410403028f610aac68566591c20f04c8
Vicary Archangel <vicrry@yahoo.com.hk>
2011-12-05 22:22:24 +0800

    Updated initial description and other tiny tinkering.

--- ini_optimize.js
+++ ini_optimize.js
@@ -1,22 +1,24 @@
-/* trim_ini.js
+/* ini_optimize.js
  *
- * '/sybase/ini/sql.ini'
+ * Work as a command line process to optimize a bulky, 
+ * yet difficult to find duplicated entries, ini file.
+ *
+ * This is originally created for optimizing the sybase 
+ * ini which had been sharing across my team for years.
+ *
+ * If you think of any feature suggestions, feel free 
+ * to let me know on github.
  *
  * @author Vicary Archangel
  */
 
 ;(function() {
 
-var argv = process.argv;
-
-if (argv[0] == 'node') {
-	argv.shift();
-}
-
-argv.shift();
+// Remove "node" and "ini_optimize.js".
+var argv = process.argv.slice(2);
 
 if (argv.length == 0) {
-	console.log("Usage: node trim_ini.js input_path[ output_path]");
+	console.log("Usage: node ini_optimize.js input_path[, output_path]");
 	
 	return;
 }
@@ -24,62 +26,71 @@ if (argv.length == 0) {
 var fs = require('fs');
 
 fs.readFile(argv[0], 'utf8', 
-function (err, data) {
-	if (err) throw err;
-	
-	var cache = {},
-		currentIndex = '',
-		reqex = /^\s*\[([^\]]+)\]\s*$/,
-		lines = data.split(/\r?\n/);
-	
-	var n = lines.length;
-	for (var i=0; i<n; i++) {
-		var line = lines[i].trim();
-		
-		if (!line||line[0] == ';') {
-			continue;
+	function (err, data) {
+		if (err) {
+			throw err;
 		}
 		
-		var matches = reqex.exec( line );
-		if (matches !== null) {
-			if (!cache.hasOwnProperty( matches[1] )) {
-				currentIndex = matches[1];
-				cache[currentIndex] = {};
+		var cache = {}, 					// Object to hold the whole ini file structure.
+			currentIndex = '',  			// Cache of tag names.
+			reqex = /^\s*\[([^\]]+)\]\s*$/, // Matcher for [...] tags. 
+			lines = data.split(/\r?\n/);	// Universal line splitter.
+		
+		var n = lines.length;
+		for (var i=0; i<n; i++) {
+			var line = lines[i].trim();
+			
+			if (!line||line[0] == ';') {
+				continue;
 			}
-		}
-		else if (currentIndex) {
-			var info = line.split( '=' );
-			if (info.length != 2) {
-				console.log( 'Invalid property type at line ' + i + ':\n' + info );
+			
+			var matches = reqex.exec( line );
+			if (matches !== null) {
+				if (!cache.hasOwnProperty( matches[1] )) {
+					currentIndex = matches[1];
+					cache[currentIndex] = {};
+				}
 			}
-			else {
-				cache[currentIndex][info[0]] = info[1].replace( /,\s*/g, ',' );
+			else if (currentIndex) {
+				var info = line.split( '=' );
+				if (info.length != 2) {
+					console.log( 'Invalid property type at line ' + i + ':\n' + info );
+				}
+				else {
+					cache[currentIndex][info[0]] = info[1].replace( /,\s*/g, ',' );
+				}
 			}
-		}
-	}
-	
-	// Get all keys
-	var result = []; for (var i in cache) result.push(i);
-	
-	// Sort the keys
-	result.sort();
-	
-	var n = result.length;
-	for (var i=0; i<n; i++) {
-		var item = cache[result[i]];
-		var text = '';
-		for (var k in item) {
-			text += k + '=' + item[k] + '\n';
 		}
 		
-		result[i] = '\n[' + result[i] + ']\n' + text;
-	}
-	
-	fs.writeFile(argv[1] || 'result.txt', result.join(''), 'utf8', 
-	function (err) {
-		if (err) throw (err);
-		else console.log('File saved.');
+		// Get all keys
+		var result = []; for (var i in cache) result.push( i );
+		
+		// Sort the keys alphabetically
+		result.sort();
+		
+		// Composite result in text format.
+		var n = result.length;
+		for (var i=0; i<n; i++) {
+			var item = cache[result[i]];
+			var text = '';
+			for (var key in item) {
+				text += key + '=' + item[key] + '\n';
+			}
+			
+			result[i] = '\n[' + result[i] + ']\n' + text;
+		}
+		
+		// Write to output file.
+		fs.writeFile(argv[1] || 'result.txt', 
+			result.join(''), 'utf8', 
+			function (err) {
+				if ( err ) {
+			  		throw ( err );
+				}
+				else {
+			  		console.log( 'File saved.' );
+				}
+			});
 	});
-});
 
 })();
